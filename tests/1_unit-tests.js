84a9242aa35abbede7c71efb598b126d9c9c121e
rsheppard83 <rsheppard83@gmail.com>
2021-06-21 22:14:58 +0100

    Compare the Properties of Two Elements

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -63,7 +63,7 @@ suite('Unit Tests', function () {
     test('#isAbove, #isAtMost', function () {
       assert.isAbove('hello'.length, 5);
       assert.isAtMost(1, 0);
-      assert.isAtMost(Math.PI, 3);
+      assert.isAbove(Math.PI, 3);
       assert.isAbove(1 - Math.random(), 1);
     });
     // #9
