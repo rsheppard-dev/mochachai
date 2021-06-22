ddd5383df89fdde321371390a23e2438039c81e3
Roy Sheppard <70776124+rsheppard-dev@users.noreply.github.com>
2021-06-22 19:10:29 +0000

    Run Functional Tests using a Headless Browser

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -80,7 +80,7 @@ suite("Functional Tests with Zombie.js", function () {
         browser.assert.success()
         browser.assert.text('span#name', 'Cristoforo')
         browser.assert.text('span#surname', 'Colombo')
-        browser.assert.element('span#dates', 1)
+        browser.assert.elements('span#dates', 1)
 
         done();
       });
